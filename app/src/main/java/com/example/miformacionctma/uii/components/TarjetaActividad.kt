0d37bc069970001603ae271d83a76311f4b21eb4
Andres-Betancur-Castañeda <ac20061234@gmail.com>
2026-08-14 07:00:55 -0500

    Nueva funcionalidad individual - Tarjetas desplegables

--- app/src/main/java/com/example/miformacionctma/uii/components/TarjetaActividad.kt
+++ app/src/main/java/com/example/miformacionctma/uii/components/TarjetaActividad.kt
@@ -17,7 +17,6 @@ import androidx.compose.foundation.layout.padding
 
 import androidx.compose.material3.Card
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
