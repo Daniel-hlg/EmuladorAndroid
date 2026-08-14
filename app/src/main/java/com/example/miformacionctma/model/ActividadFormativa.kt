3019be19e5938c849ed675fceda33655fc392190
Samuel-Rendon-Loaiza <samuelrendonloaiza7@gmail.com>
2026-08-14 07:16:17 -0500

    Guia 3 - diseño

---
+++ app/src/main/java/com/example/miformacionctma/model/ActividadFormativa.kt
@@ -0,0 +1,10 @@
+package com.example.miformacionctma.model
+
+data class ActividadFormativa(
+    val id: Int,
+    val titulo: String,
+    val descripcion: String,
+    val fecha: String,
+    val estado: String,
+    val progreso: Int
+)
